5bada5b173cfe33ff6021e65a7d8c436af348805
Jair Gómez Vásquez <jair.gomezv06@anahuac.mx>
2021-05-18 22:20:20 -0500

    Update Dijkstra
    Ahorita esta completo, pero funciona con la funcion hecha por networkX

--- dijkstra.py
+++ dijkstra.py
@@ -1,3 +1,9 @@
+## Ejemplo 1 = https://www.geeksforgeeks.org/dials-algorithm-optimized-dijkstra-for-small-range-weights/
+"""
+Input : edges: 14 Origen = 0, Destino 5
+Output : 0 > 7 > 6 > 5
+Costo : 11
+"""
 import algorithmx
 from algorithmx import http_server
 ###
@@ -12,12 +18,36 @@ random.seed(436)
 server = http_server(port=5050)
 canvas = server.canvas()
 canvas.size((500, 400))
+
+# Generate a random graph with random edge weights
+#G = nx.newman_watts_strogatz_graph(16, 2, 0.4, seed=537)
+G = nx.Graph()
+nx.set_edge_attributes(G, 0, 'weight')
+
+edges = int(input("Ingrese la cantidad de edges: "))
+print("Ingrese los edges en formato U V W")
+
+while edges:
+    x, y, w = [int(x) for x in input("Ingrese el edge con su peso: ").split()]
+    nx.add_path(G, [x,y])
+    G.edges[x,y]['weight'] = w;
+    edges-=1
+
+# Select the source and target
+source = int(input("Ingrese el origen: ")) #0
+target = int(input("Ingrese el destino: ")) #8
+print("Ahora recargue la página web y de click en start")
+
+#nx.set_edge_attributes(G, {e: randint(1, 20) for e in G.edges}, 'weight')
+
+texto = []
+texto.append("camino más corto: ")
+
+
+def finished(str1):
+    canvas.label('title').add(text=str1)
+
 def start():
-
-
-    # Generate a random graph with random edge weights
-    G = nx.newman_watts_strogatz_graph(16, 2, 0.4, seed=537)
-    nx.set_edge_attributes(G, {e: randint(1, 20) for e in G.edges}, 'weight')
 
     # Add nodes and edges with weight labels
     canvas.nodes(G.nodes).add()
@@ -26,15 +56,14 @@ def start():
     )
     canvas.pause(1)
 
-    # Select the source and target
-    source = 0
-    target = 8
     canvas.node(source).color('green').highlight().size('1.25x')
     canvas.node(target).color('red').highlight().size('1.25x')
     canvas.pause(1.5)
 
     # Run Dijkstra's shortest path algorithm
     path = nx.dijkstra_path(G, source, target)
+    #print(path)
+    texto.append(str(path)+",")
 
     # Animate the algorithm
     path_length = 0
@@ -43,7 +72,8 @@ def start():
 
         # Update the path length
         path_length += G[u][v]['weight']
-
+        
+        
         # Traverse the edge
         canvas.edge((u, v)).traverse('blue')
         canvas.pause(0.4)
@@ -59,7 +89,18 @@ def start():
         )
         canvas.pause(0.4)
 
-start()
+    texto.append("Tamaño del camino:")
+    texto.append(str(path_length))
+    str1 = ' '.join(texto)
+    print(str1)
+    finished(str1)
+
+
+canvas.onmessage('finished', finished)
+canvas.message('finished')
+
+
+#start()
 canvas.onmessage('start', start)
 
 # Start the server, blocking all further execution on
