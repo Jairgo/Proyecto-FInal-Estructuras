c6f0e4bd3410c833f3da498e03f20bc4ae72e961
Jair Gómez Vásquez <jair.gomezv06@anahuac.mx>
2021-05-19 03:06:14 -0500

    Add y Update linked list

--- Linked-List.py
+++ Linked-List.py
@@ -7,50 +7,34 @@ import networkx as nx
 server = http_server(port=5050)
 canvas = server.canvas()
 
+
+m = 1
+x = 0
+j=int(input("Digita el tamaño de lista: "))
+lista = []
+while j:
+    x=int(input("Digita el "+str(m) + " valor: "))
+    lista.append(x)
+    canvas.node(x).add(shape='rect',size=(20, 20),pos=lambda n, i: ((i - 2) * 50, 20))
+    x+=1
+    m+=1
+    j-=1
+
+l = len(lista)
+i=0
+while i < l-1:
+    x = lista[i]
+    y = lista[i+1]
+    canvas.edge((x,y)).add( directed = True )
+    i+=1
+print("Ahora recargue la pagina y da click en play")
     
 
 def start():
     #canvas.nodes(range(5)).add(shape='rect',size=(20, 20),pos=lambda n, i: ((i - 2) * 50, 20))
     #canvas.edges([(0, 1), (1, 2), (2, 3), (3, 4)]).add(directed=True)
-    """ canvas.node(0).add(shape='rect',size=(20, 20),pos=lambda n, i: ((i - 2) * 50, 20))
+    print("El tamaño de la lista es: "+str(l))
 
-
-    canvas.node(1).add(shape='rect',size=(20, 20),pos=lambda n, i: ((i - 2) * 50, 20))
-
-
-    canvas.node(2).add(shape='rect',size=(20, 20),pos=lambda n, i: ((i - 2) * 50, 20))
-    canvas.node(3).add(shape='rect',size=(20, 20),pos=lambda n, i: ((i - 2) * 50, 20))
-
-
-    canvas.edge((0,1)).add(directed=True)
-    canvas.edge((1,2)).add(directed=True)
-    canvas.edge((2,3)).add(directed=True)
-    """
-    m = 1
-    x = 0
-    j=int(input("Digita el tamaño de lista: "))
-    o=j
-    lista = []
-    while j:
-        x=int(input("Digita el "+str(m) + " valor: "))
-        lista.append(x)
-        canvas.node(x).add(shape='rect',size=(20, 20),pos=lambda n, i: ((i - 2) * 50, 20))
-        x+=1
-        m+=1
-        j-=1
-
-
-    band = 0
-    x=0
-    for i in lista:
-        if(band ==0):
-            x = i
-            band = 1
-            break
-        else:
-            y=1
-            band=0
-            canvas.edge((x,y)).add(directed=True)
         
             
 
