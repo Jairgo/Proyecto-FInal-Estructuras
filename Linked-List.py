e14411af9c856753766bbe364e3b656fe153cdf1
MemoRT <guillermo.ramirezt15@anahuac.mx>
2021-05-19 11:13:43 -0500

    Menu implementado

--- Linked-List.py
+++ Linked-List.py
@@ -2,7 +2,7 @@ import algorithmx
 from algorithmx import http_server
 ###
 import networkx as nx
-
+import os
 # Create a new HTTP server on port 5050
 server = http_server(port=5050)
 canvas = server.canvas()
@@ -33,27 +33,41 @@ def start():
         y = lista[i+1]
         canvas.edge((x,y)).add( directed = True )
         i+=1
-    print("Ahora recargue la pagina y da click en play")
-    print("El tamaño de la lista es: "+str(l))
-    print("********* MENÚ *********")
-    print("1.-Insertar al inicio")
-    print("2.-Insertar al final")
-    print("3.-Eliminar nodo en posición")
-    print("4.-Agregar nodo en cierta posicion")
-    a = int(input("Que deseas hacer: "))
+    salir = False
+    while not salir:
+        os.system('cls')
+        print("Ahora recargue la pagina y da click en play")
+        print("El tamaño de la lista es: "+str(l))
+        print("********* MENÚ *********")
+        print("1.-Insertar al inicio")
+        print("2.-Insertar al final")
+        print("3.-Eliminar nodo en posición")
+        print("4.-Agregar nodo en cierta posicion")
+        print("5.-Salir")
+        a = int(input("Que deseas hacer: "))
 
-    if a == 1:
-        val = int(input("Ingresa valor: "))
-        insertBeg(val)
-    elif a == 2:
-        val = int(input("Ingresa valor: "))
-        insertEnd(val)
-    elif a == 3:
-        val = int(input("Ingresa posicion a eliminar: "))
-        eliminarNodo(val)
-    elif a == 4:
-        pos, val = [int(x) for x in input("Ingresa posicion y valor a agregar: ").split()]
-        insertarNodo(pos,val)
+        if a == 1:
+            val = int(input("Ingresa valor: "))
+            insertBeg(val)
+            
+        elif a == 2:
+            val = int(input("Ingresa valor: "))
+            insertEnd(val)
+            
+        elif a == 3:
+            val = int(input("Ingresa posicion a eliminar: "))
+            eliminarNodo(val)
+            
+        elif a == 4:
+            pos, val = [int(x) for x in input("Ingresa posicion y valor a agregar: ").split()]
+            insertarNodo(pos,val)
+        elif a==5:
+            salir = True
+    
+    print ("Fin")
+
+       
+
     
 
 def insertBeg(val):
@@ -64,12 +78,15 @@ def insertBeg(val):
     canvas.edge((x,y)).add( directed = True )
 
 def insertEnd(val):
-    canvas.node(val).add(shape='rect',size=(20, 20),pos=lambda n, i: ((i - 2) * 50, 20))
+    canvas.node(val).add(color='green',shape='rect',size=(20, 20),pos=lambda n, i: ((i - 2) * 50, 20))
+    canvas.pause(2.5)
+    canvas.node(val).color('#323232')
     lista.append(val)
     tam = len(lista)
     x = lista[tam-2]
     y = lista[tam-1]
     canvas.edge((x,y)).add( directed = True )
+    
 
 def eliminarNodo(val):
     temp = lista[val]
@@ -80,6 +97,7 @@ def eliminarNodo(val):
     #canvas.node(temp).remove()
     canvas.node(temp).color('red').pause(2.5).remove().pause(2)
     canvas.edge((x,y)).add( directed = True )
+    lista.remove(temp)
 
 def insertarNodo(pos,val):
     canvas.node(val).add(shape='rect',size=(20, 20),pos=lambda n, i: ((i - 2) * 50, 20))
